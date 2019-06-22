3af97165069f8ae076e03a29c649b2d7b3095f4c
Michael Bardoň <wolfovelp@gmail.com>
2019-06-22 23:36:11 +0200

    msm8996-common: bluetooth: cleanup of unused variables

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -40,7 +40,6 @@ inline const char* BtmGetDefaultName()
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #define BLE_VND_INCLUDED  TRUE
-#define BTM_WBS_INCLUDED TRUE /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED TRUE /* Use WBS */
 
 #endif
